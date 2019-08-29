8d7d71b39846f9509df34eae916b3a4219e6464f
Chris White <cxwembedded@gmail.com>
2019-08-28 22:11:12 -0400

    Initial from minil

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sub::Multi::Tiny - It's new $module
+
+# SYNOPSIS
+
+    use Sub::Multi::Tiny;
+
+# DESCRIPTION
+
+Sub::Multi::Tiny is ...
+
+# LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Chris White <cxwembedded@gmail.com>
---
+++ lib/Sub/Multi/Tiny.pm
@@ -0,0 +1,39 @@
+package Sub::Multi::Tiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::Multi::Tiny - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sub::Multi::Tiny;
+
+=head1 DESCRIPTION
+
+Sub::Multi::Tiny is ...
+
+=head1 LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Chris White E<lt>cxwembedded@gmail.comE<gt>
+
+=cut
+
