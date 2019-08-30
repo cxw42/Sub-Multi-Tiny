817723e7a1529562de436c9e8fcd828b1fcd81bf
Chris White <cxwembedded@gmail.com>
2019-08-29 22:33:38 -0400

    Added attribute handler and framework
    Still need to fill in spec parsing and making the dispatcher.

--- lib/Sub/Multi/Tiny.pm
+++ lib/Sub/Multi/Tiny.pm
@@ -63,8 +63,43 @@ sub _croak {
     goto &Carp::croak;
 }
 
-# Subs we need to patch up at CHECK time
-my %_patches;
+# Information about the multisubs so we can create the dispatchers at
+# CHECK time.
+my %_multisubs;
+
+# Sanity check: any :M will die after the CHECK block below runs.
+my $_did_check_run;
+
+# CHECK: Fill in the dispatchers for any multisubs we've created.
+# Note: attributes are applied at CHECK time.  TODO see if this happens
+# late enough to work.
+
+CHECK {
+    say "In CHECK block";
+    $_did_check_run = 1;
+    while(my ($multisub_fullname, $hr) = each(%_multisubs)) {
+        my $dispatcher = _make_dispatcher($hr)
+            or die "Could not create dispatcher for $multisub_fullname\()";
+
+        #my $multi_package = $hr->{defined_in} . '::';
+        #my $stash = do { no strict 'refs'; \%{$multisub_fullname} };
+        eval { no strict 'refs'; *{$multisub_fullname} = $dispatcher };
+        die "Could not assign dispatcher for $multisub_fullname\:\n$@" if $@;
+        #_croak "Could not load stash for $multi_package" unless $stash;
+
+#        foreach my $varname (keys %$stash) {
+#            say "Checking key $varname";
+#            next unless $varname =~ /^[a-z]/;
+#            next unless do { no strict 'refs';
+#                                defined &{$multisub_fullname . $varname} };
+#                # Thanks to zentara,
+#                # https://www.perlmonks.org/?node_id=697760
+#
+#            say "Found implementation $varname";
+#        }
+
+    } #foreach multisub
+}
 
 sub import {
     my $multi_package = caller;     # The package that defines the multisub
@@ -76,7 +111,7 @@ sub import {
         unless $target_package && $subname;
 
     _croak "Can't redefine multi sub $multi_package\()"
-        if exists $_patches{$multi_package};
+        if exists $_multisubs{$multi_package};
 
     # Make a stub that we will redefine later
     say "Making $multi_package\()";
@@ -85,13 +120,16 @@ sub import {
 
     # Add in the vars - they will be accessed as package parameters
     _croak "Please list the sub parameters" unless @_;
-    vars->import::into($multi_package, @_);
+    my @possible_params = @_;
+    vars->import::into($multi_package, @possible_params);
 
     # Save the patch
-    $_patches{$multi_package} = {
+    $_multisubs{$multi_package} = {
         used_by => $target_package,
         defined_in => $multi_package,
         subname => $subname,
+        possible_params => [@possible_params],
+        impls => [],    # Implementations - subs tagged :M
     };
 
     # Set up the :M attribute in $multi_package if it doesn't
@@ -103,10 +141,68 @@ sub import {
     }
 } #import()
 
+#=head2 _line_mark_string
+#
+#Add a C<#line> directive to a string.  Usage:
+#
+#    my $str = _line_mark_string <<EOT ;
+#    $contents
+#    EOT
+#
+#or
+#
+#    my $str = _line_mark_string __FILE__, __LINE__, <<EOT ;
+#    $contents
+#    EOT
+#
+#In the first form, information from C<caller> will be used for the filename
+#and line number.
+#
+#The C<#line> directive will point to the line after the C<_line_mark_string>
+#invocation, i.e., the first line of <C$contents>.  Generally, C<$contents> will
+#be source code, although this is not required.
+#
+#C<$contents> must be defined, but can be empty.
+#
+#=cut
+
+sub _line_mark_string {
+    my ($contents, $filename, $line);
+    if(@_ == 1) {
+        $contents = $_[0];
+        (undef, $filename, $line) = caller;
+    } elsif(@_ == 3) {
+        ($filename, $line, $contents) = @_;
+    } else {
+        _croak "Invalid invocation";
+    }
+
+    _croak "Need text" unless defined $contents;
+    die "Couldn't get location information" unless $filename && $line;
+
+    $filename =~ s/"/-/g;
+    ++$line;
+
+    return <<EOT;
+#line $line "$filename"
+$contents
+EOT
+} #_line_mark_string()
+
+# Parse the argument list to the attribute handler
+sub _parse_arglist {
+    my ($spec, $funcname) = @_;
+    _croak "Need a parameter spec for $funcname" unless $spec;
+    say "Parsing args for $funcname: $spec";
+
+    # TODO RESUME HERE - parse the parameter specification and return it
+} #_parse_arglist
+
 # Create the source for the M attribute handler for a given package
 sub _make_M {
     my $multi_package = shift;
-    my $code = "package $multi_package;\n#line " . (__LINE__+1) . ' ' . __FILE__ . "\n" . <<'EOT';
+    my $code = "package $multi_package;\n";
+    $code .= _line_mark_string <<'EOT';
 use Attribute::Handlers;
 use Data::Dumper;
 
@@ -114,41 +210,47 @@ sub M :ATTR(CODE,RAWDATA) {
     print "In ", __PACKAGE__, "::M: \n", Dumper(\@_);
     my ($package, $symbol, $referent, $attr, $data, $phase,
         $filename, $linenum) = @_;
+    my $funcname = "$package\::" . *{$symbol}{NAME};
     print STDERR
         ref($referent), " ",
-        *{$symbol}{NAME}, " ",
+        $funcname, " ",
         "($referent) ", "was just declared ",
         "and ascribed the ${attr} attribute ",
         "with data ($data)\n",
         "in phase $phase\n",
         "in file $filename at line $linenum\n";
+
+    die "CHECK already ran - please file a bug report" if $_did_check_run;
+EOT
+
+    # Parse and validate the args
+    $code .= _line_mark_string <<EOT;
+    my \$args = @{[__PACKAGE__]}\::_parse_arglist(\$data, \$funcname);
+    # TODO add code to validate the args against {possible_params}
+EOT
+
+    # Save the implementation's coderef and the parsed args
+    # for use when making the dispatcher.
+    $code .= _line_mark_string <<EOT;
+    push \@{\$_multisubs{'$multi_package'}->{impls}}, [\$referent, \$args];
+EOT
+
+    $code .= _line_mark_string <<'EOT';
 } #M
 EOT
 
+    #print STDERR "M code:\n$code\n";
+    return $code;
 } #_make_M
 
-# INIT: Fill in the dispatchers for any multisubs we've created.
-# In INIT because attributes are applied at CHECK time.
-
-INIT {
-    while(my ($multi_package, $hr) = each(%_patches)) {
-        say "Patching $hr->{subname} (not really)";
-        my $multi_package = $hr->{defined_in} . '::';
-        my $stash = do { no strict 'refs'; \%{$multi_package} };
-        _croak "Could not load stash for $multi_package" unless $stash;
-
-        foreach my $varname (keys %$stash) {
-            say "Checking key $varname";
-            next unless $varname =~ /^[a-z]/;
-            next unless do { no strict 'refs';
-                                defined &{$multi_package . $varname} };
-                # Thanks to zentara,
-                # https://www.perlmonks.org/?node_id=697760
-
-            say "Found implementation $varname";
-        }
-    }
-}
+# Create a dispatcher
+sub _make_dispatcher {
+    my $hr = shift;
+    use Data::Dumper;
+    say "Making dispatcher for: ", Dumper($hr);
+    die "No implementations given for $hr->{defined_in}"
+        unless @{$hr->{impls}};
+} #_make_dispatcher
 
 1;
 # Rest of the documentation {{{1
